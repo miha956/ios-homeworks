84ee71fe259d609406dbf5996eec0d047569659e
MihaelNevash <mihaelnevash@gmail.com>
2023-11-16 22:25:38 +0300

    Initial Commit

---
+++ Navigation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Миша Вашкевич on 16.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
